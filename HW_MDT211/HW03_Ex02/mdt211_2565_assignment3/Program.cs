dfe24b4da87181001f72c7dec059212c7c578e23
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-11-19 22:50:46 +0700

    Add files via upload

---
+++ HW_MDT211/HW03_Ex02/mdt211_2565_assignment3/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+              
+    }
+    static char Inputcharacter()
+    {
+        return char.Parse(Console.ReadLine());
+    }
+}
